b2641ac5c0b192f100df382314f5e5469f536647
Mike Schutte <tmikeschutte@gmail.com>
2018-04-14 10:27:56 -0600

    Add Rider associations to RouteStop

--- app/models/route_stop.rb
+++ app/models/route_stop.rb
@@ -3,6 +3,12 @@
 class RouteStop < ApplicationRecord
   belongs_to :bus_route
   belongs_to :bus_stop
+  has_many :pickups,
+           class_name: 'Rider',
+           foreign_key: 'pickup_route_stop_id'
+  has_many :dropoffs,
+           class_name: 'Rider',
+           foreign_key: 'dropoff_route_stop_id'
 
   validates_presence_of :stop_time
 end
--- spec/models/route_stop_spec.rb
+++ spec/models/route_stop_spec.rb
@@ -5,6 +5,8 @@ require 'rails_helper'
 RSpec.describe RouteStop do
   it { should belong_to(:bus_route) }
   it { should belong_to(:bus_route) }
+  it { should have_many(:pickups) }
+  it { should have_many(:dropoffs) }
   it { should have_db_column(:stop_time) }
   it { should validate_presence_of(:stop_time) }
 end
