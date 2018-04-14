db69e7b3128d30915d44292f37f063c7f0295479
Mike Schutte <tmikeschutte@gmail.com>
2018-04-14 08:26:34 -0600

    Add bus route importer spec

---
+++ spec/models/bus_route_importer_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require 'csv'
+
+RSpec.describe BusRouteImporter do
+  describe '.main' do
+    it 'adds riders, bus_routes, and bus_stops to the db' do
+      test_rake_task = lambda do
+        read = lambda { |filename|
+          CSV.read(filename,
+                   headers: true,
+                   header_converters: :symbol)[0...1]
+        }
+        main = ->(rows) { BusRouteImporter.main(rows) }
+        pipe.call(read, main).call('./db/bus_route_data.csv')
+      end
+
+      expect { test_rake_task.call }
+        .to change { Rider.count }
+        .by(1)
+        .and change { BusStop.count }
+        .by(1)
+        .and change { BusRoute.count }
+        .by(2)
+    end
+  end
+end
