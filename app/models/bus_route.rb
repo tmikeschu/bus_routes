c3089985b26728ca918aed73b5b2bd3c6eda7b2b
Mike Schutte <tmikeschutte@gmail.com>
2018-04-14 10:03:43 -0600

    Update BusRoute model spec

--- app/models/bus_route.rb
+++ app/models/bus_route.rb
@@ -3,4 +3,6 @@
 class BusRoute < ApplicationRecord
   has_many :route_stops
   has_many :bus_stops, through: :route_stops
+
+  validates_presence_of :uuid
 end
--- spec/models/bus_route_spec.rb
+++ spec/models/bus_route_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe BusRoute do
-  it { should have_db_column(:uuid) }
   it { should have_many(:route_stops) }
   it { should have_many(:bus_stops).through(:route_stops) }
+  it { should have_db_column(:uuid) }
+  it { should validate_presence_of(:uuid) }
 end
