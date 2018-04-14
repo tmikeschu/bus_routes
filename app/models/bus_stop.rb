7cb85fe1ed281f40e964d0f4c1eff0f59a4162ec
Mike Schutte <tmikeschutte@gmail.com>
2018-04-14 10:05:32 -0600

    Update BusStop and model spec

--- app/models/bus_stop.rb
+++ app/models/bus_stop.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 class BusStop < ApplicationRecord
+  has_many :route_stops
+  has_many :bus_routes, through: :route_stops
+
+  validates_presence_of :address
 end
--- spec/models/bus_stop_spec.rb
+++ spec/models/bus_stop_spec.rb
@@ -3,7 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe BusStop do
-  it { should have_db_column(:address) }
   it { should have_many(:route_stops) }
   it { should have_many(:bus_routes).through(:route_stops) }
+  it { should have_db_column(:address) }
+  it { should validate_presence_of(:address) }
 end
