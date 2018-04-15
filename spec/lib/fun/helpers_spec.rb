44d73be8a712175a4a68bd8bb832f3e25de42409
Mike Schutte <tmikeschutte@gmail.com>
2018-04-15 16:25:01 -0400

    Add unit tests for monad helpers

--- spec/lib/fun/helpers_spec.rb
+++ spec/lib/fun/helpers_spec.rb
@@ -50,4 +50,71 @@ RSpec.describe 'Functional programming helpers' do
       end
     end
   end
+
+  describe '#fold' do
+    it 'passes proc to foldable' do
+      just_foldable    = FUN::Maybe.from_nil.call(10)
+      nothing_foldable = FUN::Maybe.from_nil.call(nil)
+
+      folder = fold.call(just: ->(x) { x * x },
+                         nothing: ->(_) { 'Error' })
+
+      expect(folder.call(just_foldable)).to eq 100
+      expect(folder.call(nothing_foldable)).to eq 'Error'
+    end
+  end
+
+  describe '#map' do
+    it 'passes proc to mappable' do
+      just_mappable    = FUN::Maybe.from_nil.call(10)
+      nothing_mappable = FUN::Maybe.from_nil.call(nil)
+      native_mappable  = [1, 7]
+
+      mapper = map.call(->(x) { x + 5 })
+
+      expect(mapper.call(just_mappable).fold).to eq 15
+      expect(mapper.call(nothing_mappable).fold).to eq nil
+      expect(mapper.call(native_mappable)).to eq [6, 12]
+    end
+  end
+
+  describe '#flat_map' do
+    it 'passes proc to flat_mappable' do
+      just_flat_mappable    = FUN::Maybe.from_nil.call(10)
+      nothing_flat_mappable = FUN::Maybe.from_nil.call(nil)
+      native_flat_mappable  = [1, 7]
+
+      flat_mapper = flat_map.call(->(x) { x + 5 })
+
+      expect(flat_mapper.call(just_flat_mappable)).to eq 15
+      expect(flat_mapper.call(nothing_flat_mappable).fold).to eq nil
+      expect(flat_mapper.call(native_flat_mappable)).to eq [6, 12]
+    end
+  end
+
+  describe '#reduce' do
+    it 'passes init value and proc to reduceable' do
+      reduceable = [1, 7]
+      reducer    = reduce(0).call(->(acc, x) { acc + x })
+      actual     = reducer.call(reduceable)
+      expected   = 8
+      expect(actual).to eq expected
+    end
+  end
+
+  describe '#try_catch' do
+    let(:divide_by_10) { ->(x) { x / 10 } }
+    it 'returns a Just wrapped value for success' do
+      actual     = try_catch.call(-> { divide_by_10.call(100) }).fold
+      expected   = 10
+      expect(actual).to eq expected
+    end
+
+    it 'returns a Nothing wrapped value for failure' do
+      actual   = try_catch.call(-> { divide_by_10.call('DHH') })
+                          .fold(nothing: ->(e) { e.to_s })
+      expected = "undefined method `/' for \"DHH\":String"
+      expect(actual).to eq expected
+    end
+  end
 end
