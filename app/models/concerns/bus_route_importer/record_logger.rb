1f1831385500f740dfc817f7b9585e95b42ffdc7
Mike Schutte <tmikeschutte@gmail.com>
2018-04-15 20:42:14 -0400

    Refactor record logger

--- app/models/concerns/bus_route_importer/record_logger.rb
+++ app/models/concerns/bus_route_importer/record_logger.rb
@@ -11,25 +11,26 @@ module BusRouteImporter::RecordLogger
 
   def group_record_types
     lambda do |row|
-      init = { bus_stop: Set.new, route: Set.new, route_stop: Set.new, rider: Set.new }
-      row.each_with_object(init) do |(k, v), acc|
-        key = case k.to_s
-              when /bus_stop/ then :bus_stop
-              when /route_id/ then :route
-              when /route_stop/ then :route_stop
-              when /rider/ then :rider
-              end
-        acc[key] << v
+      init_groups = { bus_stop: Set.new, route: Set.new,
+                      route_stop: Set.new, rider: Set.new }
+      row.reduce(init_groups) do |acc, (k, v)|
+        key = get_key_category(k)
+        acc.merge(key => acc[key].clone.add(v))
       end
     end
   end
 
-  def tally_records
-    lambda do |acc, row|
-      acc.merge(row) do |_, set1, set2|
-        set1.merge(set2)
-      end
+  def get_key_category(k)
+    case k.to_s
+    when /bus_stop/ then :bus_stop
+    when /route_id/ then :route
+    when /route_stop/ then :route_stop
+    when /rider/ then :rider
     end
+  end
+
+  def tally_records
+    ->(acc, row) { acc.merge(row) { |_, set1, set2| set1.merge(set2) } }
   end
 
   def print_results
